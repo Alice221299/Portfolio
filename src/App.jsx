22d524afe6df0b5e7b650d3f143d2c3d1b4388bb
Alice <ann.bondarets22@gmail.com>
2023-10-25 12:09:52 -0500

    :tada:Started working on portfolio

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './App.scss'
+import Introduction from './components/introduction/Introduction'
+import Projects from './components/projects/Projects'
+import Skills from './components/skills/Skills'
+import About_me from './components/about-me/About_me'
+
+function App() {
+
+  return (
+    <>
+        <header>
+
+        </header>
+        <main>
+          <Introduction/>
+          <Projects/>
+          <Skills/>
+          <About_me/>
+        </main>
+        <footer>
+
+        </footer>
+    </>
+  )
+}
+
+export default App
