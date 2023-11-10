ccc9c0af64482ea581f51c5882cd312c4272cba6
Alice <ann.bondarets22@gmail.com>
2023-11-10 16:55:48 -0500

    :construction:Working on the main part of the page

--- src/components/about-me/About_me.jsx
+++ src/components/about-me/About_me.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './about_me.scss'
+import cv from "/public/CV.pdf"
 
 const About_me = () => {
+
+  const [click, setClick] = useState(false)
   return (
-    <div>About_me</div>
+    <section className='about-me'>
+      <div>
+        <h2>A bit <span>about me</span> </h2>
+        <p>I am a frontend developer who is passionate about <span>creating beautiful and accessible websites</span>. I always strive to improve my knowledge and skills. In my free time I love doing <span>photography, cooking and travelling.</span></p>
+        <p className='about-me--download'>If you want to see more,</p>
+        {/* <button className='about-me--download-button'>Download my resume</button> */}
+        <div class="container">
+	<label>
+		{/* <input class="check" type="checkbox"/> */}
+		<div className={ click ? 'btn btn-new' : 'btn'}>
+      <a href={cv} download='CV-Anna-Bondarets' target='_blank' rel='' onClick={() => setClick(true)}>
+        <span className="download-text">Download my resume</span>
+      </a>
+			<span className="download-process">Hang on a moment, download in progress</span>
+		</div>
+	</label>
+</div>
+      </div>
+      <figure className='about-me--image'>
+        <img src="/public/images/other-image.jpg" alt="Image for hobby" />
+      </figure>
+    </section>
   )
 }
 
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,8 +1,54 @@
 import React from 'react'
+import "./projects.scss"
 
 const Projects = () => {
+
+  const projects = [{
+    image: '/public/images/Detalleando.PNG',
+    name: 'Detalleando',
+    description: 'It is a website for gift shops. It shows said shops, lets a client make an order choosing time, date and direction, communicate with a seller using chat, add products/shops to favourites, edit profile, sort and search products and shops. It also has part for seller, where he can edit shop info, check sales and change order status, check and use chat and edit/add/delete products. All the information is stored in Firebase.',
+    tech: 'React.js, React Redux, Firebase, Sass',
+    linkSite: 'https://detalleando-project.web.app/',
+    linkGitHub: 'https://github.com/MAKAIABootcamp/detalleando-project-front-5'
+  },
+  {
+    image: '/public/images/CineColombia.PNG',
+    name: 'CineColombia clone',
+    description: 'It is a simulation of the real website of Cine Colombia with a page for administrator. User can choose mmovies, filter them by genre, cinema or day and make a purchase. Administrator can edit/add/delete shows and see all the information about the movie.',
+    tech: 'React.js, Sass, React Router DOM, HTTPS requests with axios, JSON server, Git and Github',
+    linkSite: 'https://cine-colombia-fake.vercel.app/',
+    linkGitHub: 'https://github.com/Mrcsbda/Cine-Colombia-Fake'
+  },{
+    image: '/public/images/SpaceTravel.PNG',
+    name: 'Space Tourism',
+    description: '',
+    tech: '',
+    linkSite: 'https://workshop4-ten.vercel.app/',
+    linkGitHub: ''
+  }
+]
   return (
-    <div>Projects</div>
+    <section className='projects'>
+      <h2>Selected <span>projects</span></h2>
+      <div className='projects-container'>
+        {
+          projects.map((project, index) => (
+            <div key={index} className='project'>
+              <img src={project.image} alt={project.name} />
+              <div className='project-info'>
+                <h3>{project.name}</h3>
+                <p className='project-info--description'>{project.description}</p>
+                <p className='project-info--tecnologies'>{project.tech}</p>
+                <div className='project-info--links'>
+                    <a href={project.linkSite}>Live Preview</a>
+                    <a href={project.linkGitHub}>See Code</a>
+                </div>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+    </section>
   )
 }
 
